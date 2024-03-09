01381aec5938655376e9f7f486d4f11f4381d352
Limon <limonreza3.1416@gmail.com>
2024-03-09 21:43:26 +0600

    1.click effect problem solve 2.Added listener

--- app/src/main/java/com/torchtech/customcheckbox/MainActivity.kt
+++ app/src/main/java/com/torchtech/customcheckbox/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.torchtech.customcheckbox
 
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.CompoundButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val processor = findViewById<CheckBox>(R.id.processor)
+        val mainBoard = findViewById<CheckBox>(R.id.mainBoard)
+        val ram = findViewById<CheckBox>(R.id.ram)
+        val monitor = findViewById<CheckBox>(R.id.monitor)
+        val keyboard = findViewById<CheckBox>(R.id.keyboard)
+        val display = findViewById<TextView>(R.id.display)
+
+        val checkboxChangeListener = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            when (buttonView.id){
+
+                R.id.processor -> display.append("Processor\n")
+                R.id.ram -> display.append("Ram\n")
+                R.id.mainBoard -> display.append("Main-board\n")
+                R.id.monitor -> display.append("Monitor\n")
+                R.id.keyboard -> display.append("Keyboard\n")
+
+            }
+        }
+
+        processor.setOnCheckedChangeListener(checkboxChangeListener)
+        ram.setOnCheckedChangeListener(checkboxChangeListener)
+        mainBoard.setOnCheckedChangeListener(checkboxChangeListener)
+        monitor.setOnCheckedChangeListener(checkboxChangeListener)
+        keyboard.setOnCheckedChangeListener(checkboxChangeListener)
+
     }
 }
